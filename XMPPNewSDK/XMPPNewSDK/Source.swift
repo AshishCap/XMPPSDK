60eb0b63ebc880bf14bdc19036100fdd8a576ffc
Ashish <ashish.rana@capanicus.com>
2019-05-30 15:47:45 +0530

    Source commit

---
+++ XMPPNewSDK/XMPPNewSDK/Source.swift
@@ -0,0 +1,20 @@
+//
+//  Source.swift
+//  XMPPNewSDK
+//
+//  Created by ashish on 5/30/19.
+//  Copyright © 2019 Capanicus. All rights reserved.
+//
+
+import Foundation
+
+public class Service
+{
+    private init() {}
+    
+    public static func FirstMethod() -> String
+    {
+        return "Project is working"
+    }
+}
+
---
+++ XMPPNewSDK/XMPPNewSDK/XMPPNewSDK.h
@@ -0,0 +1,19 @@
+//
+//  XMPPNewSDK.h
+//  XMPPNewSDK
+//
+//  Created by ashish on 5/30/19.
+//  Copyright © 2019 Capanicus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for XMPPNewSDK.
+FOUNDATION_EXPORT double XMPPNewSDKVersionNumber;
+
+//! Project version string for XMPPNewSDK.
+FOUNDATION_EXPORT const unsigned char XMPPNewSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <XMPPNewSDK/PublicHeader.h>
+
+
